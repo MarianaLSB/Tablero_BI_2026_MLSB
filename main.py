72225a0039c2dad2a45cfce675c82b96cd5a26c2
MarianaLSB <143541156+MarianaLSB@users.noreply.github.com>
2026-03-09 20:54:08 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 # Sección de importación de módulos
 from Modules.UI.header import show_header
 from Modules.DATA.ecobici_service import EcobiciService
+import streamlit as st
+import pandas as pd
 
 # Sección para crear la GUI
 show_header("Mi primera GUI en Streamlit")
 
-st.write(EcobiciService())
+ecobici = EcobiciService()
+# Cargar datos
+df = ecobici.get_full_data()
+
+st.write(df())
